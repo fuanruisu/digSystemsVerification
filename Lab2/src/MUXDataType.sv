85d2442704d506db358a6374b838b518133fe56b
fuanruisu <56000724+fuanruisu@users.noreply.github.com>
2021-11-11 18:56:20 -0600

    Update MUXDataType.sv

--- Lab2/src/MUXDataType.sv
+++ Lab2/src/MUXDataType.sv
@@ -1,7 +1,7 @@
+
+module MUXParamType #( parameter SEL =2, parameter type IN = busin , OUT = widthData )(select, Din, Dout);
 typedef logic [2**2-1:0][1:0] busin;
 typedef logic [1:0] widthData;
-module MUXParamType #( parameter SEL =2, parameter type IN = busin , OUT = widthData )(select, Din, Dout);
-
 input logic [SEL-1:0] select;
 input IN 	Din;
 output OUT	Dout;
